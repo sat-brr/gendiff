3e6de07a8961f802a5682de3b2190972722050ff
vlad moryakov <sat1sfy@vk.com>
2022-06-22 15:57:12 +0300

    fixes

--- gendiff/formatters/json.py
+++ gendiff/formatters/json.py
@@ -1,21 +1,5 @@
 import json
 
 
-def sorted_dict(source):
-    temp_dict = {}
-    for key, item in (sorted(source.items())):
-        new_dict = {}
-        flag = item[0]
-        values = item[1:]
-        value = values[0]
-        if flag == 'nested':
-            new_dict[key] = (flag, sorted_dict(value))
-        else:
-            new_dict[key] = item
-        temp_dict.update(new_dict)
-    return temp_dict
-
-
-def to_json(source):
-    result = sorted_dict(source)
-    return json.dumps(result, indent=4)
+def to_json(diff):
+    return json.dumps(diff, sort_keys=True, indent=4)
--- tests/test_gdiff.py
+++ tests/test_gdiff.py
@@ -1,21 +1,24 @@
 import pytest
+import os
 from gendiff.diff_generator import generate_diff
 
-TEST_PATH = './tests'
+
+TEST_PATH = os.path.dirname(os.path.abspath(__file__))
 FIXTURE_PATH = f'{TEST_PATH}/fixtures'
-RESULT_STYLISH = f'{FIXTURE_PATH}/complete_stylish.txt'
-RESULT_PLAIN = f'{FIXTURE_PATH}/complete_plain.txt'
-RESULT_JSON = f'{FIXTURE_PATH}/complete_json.txt'
 
 
-@pytest.mark.parametrize('file1, file2, res_path, format',
-                         [('file5.json', 'file6.json', RESULT_STYLISH, 'stylish'),
-                          ('file5.yaml', 'file6.yaml', RESULT_PLAIN, 'plain'),
-                          ('file5.json', 'file6.yaml', RESULT_JSON, 'json')])
-def test_generate_diff(file1, file2, res_path, format):
+@pytest.mark.parametrize('file1, file2, res_file, format',
+                         [('file5.json', 'file6.json',
+                          'complete_stylish.txt', 'stylish'),
+                          ('file5.yaml', 'file6.yaml',
+                          'complete_plain.txt', 'plain'),
+                          ('file5.json', 'file6.yaml',
+                          'complete_json.txt', 'json')])
+def test_generate_diff(file1, file2, res_file, format):
     path_file1 = f'{FIXTURE_PATH}/{file1}'
     path_file2 = f'{FIXTURE_PATH}/{file2}'
+    result_path = f'{FIXTURE_PATH}/{res_file}'
     result_gendiff = generate_diff(path_file1, path_file2, format)
-    with open(res_path) as file_res:
+    with open(result_path) as file_res:
         correct_result = file_res.read()
-    assert result_gendiff == correct_result
+        assert result_gendiff == correct_result
